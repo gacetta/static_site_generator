68e245e287a9a6cb3c080c75fa20dbd26b00b5a1
gacetta <michael.gacetta@gmail.com>
2025-01-30 22:28:16 -0800

    refactor: combine leafnode into htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,3 +23,23 @@ class HTMLNode():
         children: {self.children},
         props: {self.props}
     )"""
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        if value == None:
+            raise ValueError("value is required and cannot be None")
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag == None:
+            return f"{self.value}"
+        # else:
+        #     opening_tag = self.tag
+        #     if self.props != None:
+        #         opening_tag += self.props_to_html()
+        #     opening_tag = f"<{opening_tag}>"
+        #     closing_tag = f"</{self.tag}>"
+        #     return opening_tag + self.value + closing_tag
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -57,6 +57,77 @@ class TestHTMLNode(unittest.TestCase):
         result = node.props_to_html()
         self.assertEqual(result, ' href="https://www.google.com" target="_blank"')
 
+class TestLeafNode(unittest.TestCase):
+    def test_create_node(self):
+        node = LeafNode(
+            "a", 
+            "anchor node test", 
+            {
+                "href": "https://www.google.com",
+            }
+        )
+        self.assertEqual(node.tag, "a")
+        self.assertEqual(node.value, "anchor node test")
+        self.assertEqual(node.props, {
+        "href": "https://www.google.com",
+        })
+
+    def test_create_node_with_no_value(self):
+        with self.assertRaises(ValueError):
+            LeafNode("a", None)
+
+    def test_create_node_with_no_props(self):
+        node = LeafNode(
+            "a", 
+            "anchor node test",
+        )
+        self.assertEqual(node.tag, "a")
+        self.assertEqual(node.props, None)
+
+    def test_create_node_with_children(self):
+        node = LeafNode(
+            "a", 
+            "anchor node test", 
+        )
+        self.assertIsNone(node.children)
+
+    def test_to_html(self):
+        node = LeafNode(
+            "a", 
+            "anchor node test"
+        )
+        result = node.to_html()
+        self.assertIn("<a>anchor node test</a>", result)
+
+    def test_to_html_with_props(self):
+        node = LeafNode(
+            "a", 
+            "anchor node test", 
+            {
+                "href": "https://www.google.com",
+                "target": "_blank"
+            }
+        )
+        result = node.to_html()
+        self.assertIn('<a href="https://www.google.com" target="_blank">anchor node test</a>', result)
+
+    def test_to_html_with_no_tag(self):
+        node = LeafNode(
+            None, 
+            "anchor node test"
+        )
+        result = node.to_html()
+        self.assertIn("anchor node test", result)
+
+    def test_to_html_with_no_value(self):
+        node = LeafNode(
+            "a", 
+            "test"
+        )
+        node.value = None
+        with self.assertRaises(ValueError):
+            node.to_html()
+        
 
 if __name__ == "__main__":
     unittest.main()
