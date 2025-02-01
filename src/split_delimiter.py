b9676fe85a166cbc6f7ed48dce71831b4e9e24ee
gacetta <michael.gacetta@gmail.com>
2025-02-01 12:10:22 -0800

    add: split_delimiter function and tests

---
+++ src/split_delimiter.py
@@ -0,0 +1,40 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    # create list of new_nodes to return
+    new_nodes = []
+
+    # iterate over each node
+    for node in old_nodes:
+
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+
+        else:
+            text = node.text
+            delimiter_offset = len(delimiter)
+            # find positions of delimters
+            start = text.find(delimiter)
+            if start != -1:
+                second_instance = text[start+delimiter_offset:].find(delimiter)
+
+                if second_instance == -1:
+                    raise Exception("Invalid Markdown Syntax")
+                
+                end = second_instance + start + delimiter_offset
+                
+                text_1 = text[:start]
+                text_2 = text[start+delimiter_offset:end]
+                text_3 = text[end+delimiter_offset:]
+
+                new_node_1 = TextNode(text_1, TextType.TEXT)
+                new_node_2 = TextNode(text_2, text_type)
+                new_node_3 = TextNode(text_3, TextType.TEXT)
+
+                new_nodes.append(new_node_1)
+                new_nodes.append(new_node_2)
+                new_nodes.append(new_node_3)
+
+            else:
+                new_nodes.append(node)
+    return new_nodes
---
+++ src/test_split_delimiter.py
@@ -0,0 +1,43 @@
+import unittest
+from textnode import TextNode, TextType
+from split_delimiter import split_nodes_delimiter
+
+
+class TestSplitDelimiter(unittest.TestCase):
+    def test_split_nodes_delimiter_code(self):
+        node = TextNode("Hello `code` world", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[0].text, "Hello ")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[2].text, " world")
+    
+    def test_split_nodes_delimiter_bold(self):
+        node = TextNode("Hello **bold** world", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[0].text, "Hello ")
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[1].text, "bold")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[2].text, " world")
+
+    def test_split_nodes_delimiter_error_handling(self):
+        with self.assertRaises(Exception) as context:
+            node = TextNode("Hello `code without closing tick", TextType.TEXT)
+            new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+        self.assertEqual(str(context.exception), "Invalid Markdown Syntax")
+        
+        # Should raise an exception
+
+
+    # def test_connection(self):
+    #     node = TextNode("This is text with a `code block` word", TextType.TEXT)
+    #     new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    #     print(new_nodes)
+    #     self.assertEqual(new_nodes, "test")
