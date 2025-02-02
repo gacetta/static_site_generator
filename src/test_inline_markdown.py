3f5a2638e8c44b3888d827472837f27f6bc48f60
gacetta <michael.gacetta@gmail.com>
2025-02-02 11:30:08 -0800

    add: additional tests for multiple markdown

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -4,40 +4,77 @@ from inline_markdown import split_nodes_delimiter
 
 
 class TestSplitDelimiter(unittest.TestCase):
-    def test_split_nodes_delimiter_code(self):
+    def test_delim_code(self):
         node = TextNode("Hello `code` world", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 
-        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
-        self.assertEqual(new_nodes[0].text, "Hello ")
-        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
-        self.assertEqual(new_nodes[1].text, "code")
-        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
-        self.assertEqual(new_nodes[2].text, " world")
+        self.assertListEqual(
+            [
+                TextNode("Hello ", TextType.TEXT),
+                TextNode("code", TextType.CODE),
+                TextNode(" world", TextType.TEXT),
+            ],
+            new_nodes
+        )
     
-    def test_split_nodes_delimiter_bold(self):
+    def test_delim_bold(self):
         node = TextNode("Hello **bold** world", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
 
-        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
-        self.assertEqual(new_nodes[0].text, "Hello ")
-        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
-        self.assertEqual(new_nodes[1].text, "bold")
-        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
-        self.assertEqual(new_nodes[2].text, " world")
+        self.assertListEqual(
+            [
+                TextNode("Hello ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" world", TextType.TEXT),
+            ],
+            new_nodes
+        )
 
-    def test_split_nodes_delimiter_error_handling(self):
+    def test_delim_bold_multiword(self):
+        node = TextNode(
+            "This is text with a **bolded word** and **another**", TextType.TEXT
+        )
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded word", TextType.BOLD),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("another", TextType.BOLD),
+            ],
+            new_nodes,
+        )
+    
+    def test_delim_italic(self):
+        node = TextNode("Hello *italic* world", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
+
+        self.assertListEqual(
+            [
+                TextNode("Hello ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" world", TextType.TEXT),
+            ],
+            new_nodes
+        )
+    
+    def test_delim_bold_and_italic(self):
+        node = TextNode("*italic* and **bold**", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
+
+        self.assertListEqual(
+            [
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+            ],
+            new_nodes
+        )
+
+    def test_delim_error_handling(self):
         with self.assertRaises(Exception) as context:
             node = TextNode("Hello `code without closing tick", TextType.TEXT)
             new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 
-        self.assertEqual(str(context.exception), "Invalid Markdown Syntax")
-        
-        # Should raise an exception
-
-
-    # def test_connection(self):
-    #     node = TextNode("This is text with a `code block` word", TextType.TEXT)
-    #     new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-    #     print(new_nodes)
-    #     self.assertEqual(new_nodes, "test")
+        self.assertEqual(str(context.exception), "Invalid markdown, formatted section not closed")
