e89414719da9ca4b1e8f6dfbbee660148aed2a83
gacetta <michael.gacetta@gmail.com>
2025-02-02 11:02:38 -0800

    rename: split_delimiter to inline_markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,15 +1,10 @@
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    # create list of new_nodes to return
     new_nodes = []
-
-    # iterate over each node
     for node in old_nodes:
-
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
-
         else:
             text = node.text
             delimiter_offset = len(delimiter)
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from split_delimiter import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter
 
 
 class TestSplitDelimiter(unittest.TestCase):
